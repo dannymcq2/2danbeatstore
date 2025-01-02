4fb9dc502c4ac28798592fc7d8856ef5297c48b9
dannymcq2 <mcq.danny@icloud.com>
2025-01-02 18:40:54 -0500

    port fix

--- backend/server.js
+++ backend/server.js
@@ -129,6 +129,6 @@ app.post('/checkout', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on ${PORT}`);
 });
